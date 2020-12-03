9a644772fb89f3ecdc575b1b44473ff43071afbd
Jesús López <71353275+LopezJesus@users.noreply.github.com>
2020-12-02 23:35:13 -0800

    Create holamundo.c

---
+++ holamundo.c
@@ -0,0 +1,12 @@
+/* Lopez Galaz Jesus Xavier */
+/* No. Control: 18212208*/
+/* Programa  ARM64: Hola mundo*/
+/* Materia: Lenguajez de Interfaz*/
+
+
+#include <stdio.h>
+int main (void)
+{
+	printf("Hola Mundo\n");
+	return 0;
+}
